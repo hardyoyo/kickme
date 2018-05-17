a713193f1de1820a6a121b21ac34e96b04700805
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-05-17 16:24:44 -0500

    removed all remaining nurax configs, this app is kickme, not nurax

--- config/deploy/sandbox.rb
+++ config/deploy/sandbox.rb
@@ -2,5 +2,5 @@
 set :stage, :sandbox
 set :rails_env, 'production'
 set :deploy_to, '/opt/kickme'
-server 'nurax.curationexperts.com', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
+server 'kickme.curationexperts.com', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
 
--- config/deploy/travis.rb
+++ config/deploy/travis.rb
@@ -2,6 +2,6 @@
 set :stage, :travis
 set :rails_env, 'production'
 set :deploy_to, '/opt/kickme'
-server 'nurax.curationexperts.com', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
+server 'kickme.curationexperts.com', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
 set :ssh_options, keys: ['config/kickme-travis']
 
