5200e6b17d88b9185c98fbb0d2ddefa7ed865cdf
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-06-27 15:44:13 -0500

    adding Solr and Redis checks to OKComputer config

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,3 +1,6 @@
 if ENV["OKCOMPUTER_PASSWD"].present?
   OkComputer.require_authentication("status", ENV["OKCOMPUTER_PASSWD"])
 end
+
+OkComputer::Registry.register "redis", OkComputer::RedisCheck.new
+OkComputer::Registry.register "solr", OkComputer::SolrCheck.new
