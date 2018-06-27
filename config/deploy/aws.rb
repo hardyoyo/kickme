829bd15fb992a8b5cd0846b8412eed82ac0025a5
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-06-27 13:27:38 -0500

    using new temporary domain name: kickme.thebignow.com

--- config/deploy/aws.rb
+++ config/deploy/aws.rb
@@ -2,5 +2,5 @@
 set :stage, :aws
 set :rails_env, 'production'
 set :deploy_to, '/opt/kickme'
-server 'ec2-54-189-34-94.us-west-2.compute.amazonaws.com', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
+server 'kickme.thebignow.com', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
 
