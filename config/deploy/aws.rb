46965038a6d35b709058db4ec5e0c56ab62caba9
Hardy Pottinger <hardy.pottinger@gmail.com>
2018-06-28 13:57:31 -0500

    remove redis and solr checks from okcomputer, they are complicating the first_deploy role for Ansible

--- config/deploy/aws.rb
+++ config/deploy/aws.rb
@@ -1,6 +1,6 @@
-# deploys to http://ec2-54-189-34-94.us-west-2.compute.amazonaws.com
+# deploys to http://52.13.211.11/
 set :stage, :aws
 set :rails_env, 'production'
 set :deploy_to, '/opt/kickme'
-server 'kickme.thebignow.com', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
+server '52.13.211.11', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
 
